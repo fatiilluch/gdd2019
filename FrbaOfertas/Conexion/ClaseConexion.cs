77d816c7ed7f4e99e8fc8658aa7e243b0e4912ee
Fatima Lluch <fatilluch@gmail.com>
2019-11-10 15:43:38 -0300

    Agregado ABM Rol - Alta

--- FrbaOfertas/Conexion/ClaseConexion.cs
+++ FrbaOfertas/Conexion/ClaseConexion.cs
@@ -6,22 +6,40 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 using System.IO;
+using System.Security.Cryptography;
 using System.Collections;
 using System.Configuration;
+using System.Data.SqlTypes;
+using System.Data;
+using FrbaOfertas;
 
 namespace FrbaOfertas.Conexion
 {
-    class ClaseConexion
+    public class ClaseConexion
     {
-        static string server = ConfigurationManager.AppSettings["server"].ToString();
+        SqlCommand command;
+
+        public static string configuracionConexionSQL = @"Data Source= \\SQLSERVER2012;Initial Catalog=GD2C2019; Persist Security Info=True;User ID= gdCupon2019 ;PASSWORD= gd2019";
+
+        /*static string server = ConfigurationManager.AppSettings["server"].ToString();
         static string user = ConfigurationManager.AppSettings["user"].ToString();
         static string password = ConfigurationManager.AppSettings["password"].ToString();
-
+        */
         // declaro una variable de conexion global
         //public static SqlConnection conexion = new SqlConnection("Data Source=DESKTOP-A4VN5NH\\SQLSERVER2012;Initial Catalog = GD2C2019; Integrated Security=True;User ID=gdCupon2019;Password=********;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False");
-        public static SqlConnection conexion = getConnection();
+        public static SqlConnection conexion = new SqlConnection(configuracionConexionSQL);
 
-        public static void Conectar()
+        public string getConfig()
+        {
+            return configuracionConexionSQL;
+        }
+
+        public SqlConnection obtenerConexion()
+        {
+            return conexion;
+        }
+
+        public void Conectar()
         {
             try
             {
@@ -34,39 +52,117 @@ namespace FrbaOfertas.Conexion
             }
         }
 
-        public static SqlDataReader ResolverConsulta(String query)
+        public void Desconectar()
         {
-            return new SqlCommand(query, conexion).ExecuteReader();
+            conexion.Close();
         }
 
-        public static int ResolverNonQuery(String nonQuery)
+        public static SqlCommand crearConsulta(string consulta)
         {
-            return new SqlCommand(nonQuery, conexion).ExecuteNonQuery();
+            return new SqlCommand(consulta, conexion);
         }
 
-        public static object ResolverFuncion(string query)
+        public void ejecutarConsulta(string consulta)
         {
-            return new SqlCommand(query, conexion).ExecuteScalar();
+            SqlCommand query = new SqlCommand(consulta, conexion);
+            try
+            {
+                Conectar();
+                query.ExecuteNonQuery();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("No se pudo establecer la conexion a la base de datos" + error.Message);
+            }
+            Desconectar();
         }
 
-        public static void ActualizarGrid(DataGridView dg, String tabla, string consulta)
+        public int ejecutarConsultaSinResultado(SqlCommand consulta)
         {
-            // instancio el dataset que va a llenar de datos el sdatadridview
-            System.Data.DataSet ds = new System.Data.DataSet();
-            // instancio un adaptador de datos entre el dataset y la base de datos
-            SqlDataAdapter da = new SqlDataAdapter(consulta, conexion);
-            // llenar el dataset con los datos de la tabla NN a través del adapter
-            da.Fill(ds, tabla);
-            dg.DataSource = ds;
-            // para traer todo el contenido de la tabla NN al dataGridView
-            dg.DataMember = tabla;
+            Conectar();
+            int resultado = consulta.ExecuteNonQuery();
+            Desconectar();
+            return resultado;
         }
 
-        public static SqlConnection getConnection()
+        public int ejecutarConsultaDevuelveInt(SqlCommand consulta)
         {
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=" + server + "\\SQLSERVER2012;Initial Catalog=GD2C2019; Persist Security Info=True;User ID=" + user + ";PASSWORD=" + password + ";";
-            return con;
+            Conectar();
+            int resultado = consulta.ExecuteNonQuery();
+            Desconectar();
+            return resultado;
+        }
+
+        public int obtenerIntDeConsulta(string consulta)
+        {
+            SqlCommand query = new SqlCommand(consulta, conexion);
+            int entero = 0;
+            Conectar();
+            entero = query.ExecuteNonQuery();
+            Desconectar();
+            return entero;
+        }
+
+        public DataSet obtenerDataSet(SqlCommand consulta)
+        {
+            DataSet dataSet = new DataSet();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(consulta);
+            dataAdapter.Fill(dataSet);
+            return dataSet;
+        }
+
+        public DataTable obtenerDataTable(SqlCommand consulta)
+        {
+            DataSet dataSet = obtenerDataSet(consulta);
+            DataTable tabla = dataSet.Tables[0];
+            return tabla;
+        }
+
+        public List<string> obtenerListaDeDatos(SqlCommand consulta)
+        {
+            DataTable tabla = obtenerDataTable(consulta);
+            List<string> columna = new List<string>();
+            if (tabla.Rows.Count > 0)
+                foreach (DataRow fila in tabla.Rows)
+                    columna.Add(fila[0].ToString());
+            return columna;
+        }
+
+
+        public void crearSP(string nombreConsulta)
+        {
+
+            SqlCommand sp = new SqlCommand(nombreConsulta, conexion);
+            sp.CommandType = CommandType.StoredProcedure;
+        }
+
+
+        public void setearParametroPorValor(string nombre, object valor)
+        {
+            command.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void setearParametroPorReferencia(string nombre, SqlDbType tipoDato)
+        {
+            command.Parameters.Add(new SqlParameter(nombre, tipoDato));
+            command.Parameters[nombre].Direction = ParameterDirection.Output;
+        }
+
+        public int ejecutarSP()
+        {
+            try
+            {
+                command.Parameters.Add("@ReturnVal", SqlDbType.Int);
+                command.Parameters["@ReturnVal"].Direction = ParameterDirection.ReturnValue;
+                command.ExecuteNonQuery();
+                return Convert.ToInt32(command.Parameters["@ReturnVal"].Value);
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("No se pudo establecer la conexion a la base de datos" + exception.Message);
+                return -1;
+            }
         }
     }
 }
